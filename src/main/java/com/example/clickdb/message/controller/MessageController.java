89d1897393ecec85a9704c825c5dcd22812cf874
artemsavelev <savelev.artem.v@gmail.com>
2020-11-16 14:46:17 +0300

    added annotations Swagger in controller

--- src/main/java/com/example/clickdb/message/controller/MessageController.java
+++ src/main/java/com/example/clickdb/message/controller/MessageController.java
@@ -3,6 +3,8 @@ package com.example.clickdb.message.controller;
 import com.example.clickdb.message.dao.MessageDao;
 import com.example.clickdb.message.dto.MessageDto;
 import com.example.clickdb.message.model.Message;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/messages")
+@Api(value="onlineAddingMessage", description="Operations pertaining to add messages")
 public class MessageController {
 
     private final MessageDao messageDao;
@@ -27,6 +30,7 @@ public class MessageController {
     }
 
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    @ApiOperation(value = "View a list of messages", response = Iterable.class)
     public ResponseEntity<List<Message>> getAll() {
         List<Message> result = messageDao.getAll();
         return new ResponseEntity<>(result, HttpStatus.OK);
@@ -35,10 +39,12 @@ public class MessageController {
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     @RequestMapping(value = "/addAll", method = RequestMethod.POST)
+    @ApiOperation(value = "Adding a list of messages")
     public ResponseEntity<List<MessageDto>> saveAll(@RequestBody List<Message> messages) {
         if (messages.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+
         messageDao.addAll(messages);
         List<MessageDto> result = MessageDto.messageDtoList(messages);
         return new ResponseEntity<>(result, HttpStatus.OK);
